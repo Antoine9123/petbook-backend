cb3479f5f93b7cf52afdfc314127d8eea14b0ae2
Antoine-Alexandr Leytens <aa.leytens@gmail.com>
2024-05-07 16:50:58 +0200

    Add serialized to user model

--- my-app/auth/views.py
+++ my-app/auth/views.py
@@ -1,13 +1,26 @@
+import json
 from django.http import JsonResponse
+from django.forms.models import model_to_dict
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from users.models import User
+from users.serializers import UserSerializer
 
+
+@api_view(["GET"])
 def signup(request):
-    model_data =  User.objects.all()
+    instance =  User.objects.all().order_by("?").first()
     data = {}
-    if model_data:
-        data['email'] = model_data.email
-        data['username'] = model_data.username
-        data['password'] = model_data.password
-        
-    return JsonResponse(data)
+    if instance:
+        data = UserSerializer(instance).data
+    return Response(data, content_type='application/json')
+
+@api_view(["POST"])
+def signup(request):
+    instance =  User.objects.all().order_by("?").first()
+    data = {}
+    if instance:
+        data = UserSerializer(instance).data
+    return Response(data, content_type='application/json')
