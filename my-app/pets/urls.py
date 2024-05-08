1de56cb0aea06b38943ba269a37815cc013adef2
Antoine-Alexandr Leytens <aa.leytens@gmail.com>
2024-05-08 11:10:18 +0200

    format pet age + url

--- my-app/pets/urls.py
+++ my-app/pets/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.pet_list),
-    path("<int:id>/", views.pet_detail)
+    path("", views.pet_list, name='pet-list'),
+    path("<int:id>/", views.pet_detail, name='pet-detail')
 ]
