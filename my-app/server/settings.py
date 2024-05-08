0b2c2372be1c9b19c8289fcf5143b9797e8f5113
Antoine-Alexandr Leytens <aa.leytens@gmail.com>
2024-05-08 12:01:32 +0200

    add sign up and login

--- my-app/server/settings.py
+++ my-app/server/settings.py
@@ -28,7 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'api',
+    'rest_framework.authtoken',
     'users',
     "pets"
 ]
