eb73af2fbb8279b189f24ab2a9777e0113139d46
Antoine-Alexandr Leytens <aa.leytens@gmail.com>
2024-05-08 12:03:35 +0200

    delete user models created by myself

--- my-app/users/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class User(models.Model):
-    email = models.EmailField()
-    username = models.CharField(max_length=20)
-    password = models.CharField(max_length=10000)
-    description = models.TextField(blank=True, null=True)
